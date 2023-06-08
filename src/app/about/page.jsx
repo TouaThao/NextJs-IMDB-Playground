7fbeed32cf170ef682f408503aaced3292681032
TouaThao <touathao09@gmail.com>
2023-06-07 20:35:44 -0500

    nav,darkmode,theme,header

---
+++ src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>Welcome, This is a IMDB Playground</p>
+      <p>
+        IMDB Playground offers a wide-ranging movie database encompassing films
+        from across the globe. Additionally, it provides up-to-date news,
+        reviews, and trailers.
+      </p>
+      <p>
+        Thank you for visiting our website, and we sincerely hope you have a
+        delightful time exploring our movie database.
+      </p>
+    </div>
+  );
+}
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,22 @@
-import './globals.css'
-import Header from '@/components/Header'
-
+import "./globals.css";
+import Header from "@/components/Header";
+import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
 export const metadata = {
-  title: 'IMDB',
-  description: 'IMDB playground',
-}
+  title: "IMDB",
+  description: "IMDB playground",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header/>
-        {children}
-        </body>
+        <Providers>
+          <Header />
+          <Navbar />
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
